a6daec10b62339f7b4402473a144a51bc96c1923
pyrotech52 <coke.pyrotech@gmail.com>
2023-11-29 13:20:05 -0800

    all

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,10 @@
-#!/usr/bin/env python3
-print_last_digit = __import__('9-print_last_digit').print_last_digit
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number >= 0:
+        l_digit = number % 10
+    else:
+        l_digit = number % -10
+        l_digit *= -1
 
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
+    print("{:d}".format(l_digit), end='')
+    return (l_digit)
