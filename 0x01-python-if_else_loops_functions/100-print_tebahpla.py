db2a96dcde09ee396a4a47352a81eaa897f2514f
pyrotech52 <coke.pyrotech@gmail.com>
2023-11-29 14:12:22 -0800

    100-print

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,12 +1,10 @@
-1	#!/usr/bin/python3
-2	
-3	for i in range(122, 96, -1):
-4	
-5		if i % 2:
-6	
-7			i = i - 32
-8	
-9		print("{:c}".format(i), end="")
-10	
-11	
+#!/usr/bin/python3
+
+for i in range(122, 96, -1):
+
+	if i % 2:
+
+		i = i - 32
+
+	print("{:c}".format(i), end="")
 
