5bea2d1967f47a368447fb36b84d8f70af2e5b1e
pyrotech52 <coke.pyrotech@gmail.com>
2023-12-06 16:08:44 -0800

    ddd

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,5 @@ def square_matrix_simple(matrix=[]):
     new_matrix = [row[:] for row in matrix]
     for idx, row in enumerate(new_matrix):
         for idx1, col in enumerate(row):
-            new_matrix[idx][idx2] = col ** 2
+            new_matrix[idx][idx2] = row[idx2] ** 2
     return new_matrix
