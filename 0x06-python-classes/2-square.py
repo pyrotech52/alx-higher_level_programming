57b0ee1ef40329d7ecb6299c3b287b8416bcde83
pyrotech52 <coke.pyrotech@gmail.com>
2023-12-19 18:06:53 -0800

    dfd

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,20 +3,16 @@
 
 
 class Square:
-    """
-    A class representing a square.
+    """A class representing a square.
 
     Attributes:
-    - __size (int): Private instance attribute representing the size of the square.
-    """
+    - __size (int): Private instance attribute representing the size of the square."""
 
     def __init__(self, size=0):
-        """
-        Initializes a Square instance with an optional size.
+        """Initializes a Square instance with an optional size.
 
         Parameters:
-        - size (int, optional): The size of the square. Default is 0.
-        """
+        - size (int, optional): The size of the square. Default is 0."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
