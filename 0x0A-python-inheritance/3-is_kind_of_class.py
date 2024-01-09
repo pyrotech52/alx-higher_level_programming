e58a869749c048f20ba40d8691afa5b8083c7ef2
pyrotech52 <coke.pyrotech@gmail.com>
2024-01-08 16:13:24 -0800

    oxoa

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""methods module"""
+
+
+def is_kind_of_class(obj, a_class):
+    """function that returns True if the object is an instance of"""
+    return isinstance(obj, a_class)
