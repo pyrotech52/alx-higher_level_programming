0f786e3b0f33ad57856b85caf0b5c3ed7cd692d3
pyrotech52 <coke.pyrotech@gmail.com>
2024-03-22 18:42:47 -0700

    sff

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-"""Write a Python file similar to model_state.py named
-model_city.py that contains the class definition of a City"""
-
+""" prints the State object with the name passed as argument from the database
+"""
+import sys
 from model_state import Base, State
 from model_city import City
-from sqlalchemy import create_engine
-from sys import argv
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__" :
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(argv[1], argv[2], argv[3]))
-
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    Base.metadata.create_all(engine)
+    for instance in (session.query(State.name, City.id, City.name)
+                     .filter(State.id == City.state_id)):
+        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2])
 
-    states = session.query(State, City).join(State).all()
-
-    for state, city in states:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
-    session.close()
--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-"""script that lists all State objects from the database hbtn_0e_6_usa"""
-
+"""Start link class to table in database
+"""
+import sys
 from model_state import Base, State
-from sqlalchemy import create_engine
-from sys import argv
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
+
 if __name__ == "__main__":
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(argv[1], argv[2], argv[3]))
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    Base.metadata.create_all(engine)
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
 
-    states = session.query(State).order_by(State.id)
-
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
-    session.close()
